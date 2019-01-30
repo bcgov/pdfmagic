69bf568a9b587d99912a7d62f28ab12bca9e3e33
Baraka <baraka.lwakila@gov.bc.ca>
2019-01-30 10:57:29 -0800

    add downloading zips of converted files

--- scrape.py
+++ scrape.py
@@ -6,41 +6,56 @@ import pytesseract
 
 OUTPUT_FOLDER_DEFAULT = "pdf_output"
 
+#todo: make this more cross-platform?
+
+### Extract text from a pdf file using Tesseract OCR
 def ocr(pdffile,dirs):
+    # convert pdfs to images
     images = pdf2image.convert_from_path(pdffile,output_folder=dirs['img_dir'])
     text = ""
     for image in images:
+        # run ocr on image
         text += (pytesseract.image_to_string(image,lang='eng'))
         text += ('\n\n')
-
+    # this is hardcoded but these type of exceptions should be loaded in a config file
     if 'Public Comment Form' in text:
         subprocess.run(['mv',pdffile,dirs['forms']])
     else:
+        # save text file
         txtfile = dirs['ocr_dir']+'/'+((pdffile.split('/')[-1]).split('.')[0] + '.txt')
         with open(txtfile,'w') as txt:
             txt.write(text)
 
+### Scrape text from a pdf using pdftotext from Poppler
 def scrape(pdffile,dirs):
     if pdffile[-4:] != '.pdf': return
+    
+    # "good" files are text pdfs that don't need OCR
     good = False
     subprocess.run(['pdftotext',pdffile])
     print(pdffile)
     txtfile = ('.'.join(pdffile.split('.')[:-1]) + '.txt')
     print(txtfile)
+
+    # check if the .txt file is "good" (not empty)
     with open(txtfile,'r') as txt:
         if txt.read().strip():
             good = True
     if good: 
+        # save .txt of good file
         subprocess.run(['mv',txtfile,dirs['scraped']])
     else:
+        # put bad pdfs in a special dir and delete the empty .txt
         subprocess.run(['cp',pdffile,dirs['noscrape']])
         subprocess.run(['rm',txtfile])
 
+### Return list of .pdf files in a target dir
 def get_pdfs(target):
     gl = glob.glob(target + '/*.pdf')
     print(gl)
     return gl
 
+### Return arg parser object
 def init_argparser():
     parser = argparse.ArgumentParser(description="Scrapes scrapable pdfs and OCRs those that are not, separating the two.")
     parser.add_argument('target')
@@ -48,9 +63,9 @@ def init_argparser():
     parser.add_argument('-b','--batch',action='store_true')
     return parser
 
+### Return dict with all output subdirs
 def init_dirs(output_dir):
     output_dir = output_dir.strip()
-    # print(f'|{output_dir}|')
     dirs = {}
     dirs['scraped'] = output_dir + '/scraped'
     dirs['noscrape'] = output_dir + '/noscrape'
@@ -58,11 +73,13 @@ def init_dirs(output_dir):
     dirs['img_dir'] = output_dir + '/img'
     dirs['forms'] = output_dir + '/forms'
 
+    # make output dir and subdirs
     subprocess.run(['mkdir',output_dir])
     for dir in dirs.values():
         subprocess.run(['mkdir',dir])
     return dirs
 
+### Shouldn't really be called directly
 def start(args):
     output = args.output.strip('/') if args.output else OUTPUT_FOLDER_DEFAULT
     dirs = init_dirs(output)
@@ -75,7 +92,13 @@ def start(args):
 
     for pdf in get_pdfs(dirs['noscrape']):
         ocr(pdf,dirs)
+    
+    #remove temp dirs
+    subprocess.run(['rm','-r',dirs['noscrape']])
+    subprocess.run(['rm','-r',dirs['img_dir']])
 
+### This function can be called externally, 
+### its parameters create args equivalent to command-line
 def run(target,batch=False,output=OUTPUT_FOLDER_DEFAULT):
     parser = init_argparser()
     if batch: 
--- server.py
+++ server.py
@@ -2,28 +2,52 @@ import flask
 import os
 import sys
 from werkzeug.utils import secure_filename
+import shutil
+from datetime import datetime as dt
+import string
+import base64
 
 import scrape
 
 UPLOAD_FOLDER = './pdf_upload'
+SCRAPE_OUTPUT_FOLDER = './pdf_output'
 ALLOWED_EXTENSIONS = set(['pdf'])
+SID_SIZE = 30
 
 
 app = flask.Flask(__name__)
 app.secret_key = os.urandom(24)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SCRAPE_OUTPUT_FOLDER'] = SCRAPE_OUTPUT_FOLDER
 
+### Returns a boolean value
+### whether the filename has an allowed extension or not
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
 
-def batch_scrape():
-    scrape.run(app.config['UPLOAD_FOLDER'],batch=True)
+def batch_scrape(sid):
+    scrape.run(os.path.join(app.config['UPLOAD_FOLDER'],sid),batch=True,output=os.path.join(app.config['SCRAPE_OUTPUT_FOLDER'],sid))
+    print("DONE",file=sys.stderr)
+
+### zip the output dir of the scraper
+def zip_files(sid):
+    #tstamp = '-'.join('_'.join(str(dt.now()).split(' ')).split(':')).split('.')[0]
+    path = os.path.join(app.config['SCRAPE_OUTPUT_FOLDER'],sid)
+    shutil.make_archive(path,'zip',path)
+
+def generate_sid():
+    return base64.b64encode(os.urandom(24)).decode().replace('/','0')
 
 @app.route('/',methods=['GET','POST'])
 def uploader():
     req = flask.request
-    # print('here',file=sys.stderr)
+
+    ### POST requests should be file uploads
     if req.method == 'POST':
+        #generate session id
+        sid = generate_sid()
+        flask.session['sid'] = sid
+
         if 'file[]' not in req.files:
             flask.flash('No file part')
             return flask.redirect(req.url)    
@@ -33,15 +57,32 @@ def uploader():
 
         upcount = 0
 
+        # process filenames, and save files
+        upload_folder = os.path.join(app.config['UPLOAD_FOLDER'],sid)
+        os.mkdir(upload_folder)
         for _file in uploaded_files:
             if not _file: continue
             if allowed_file(_file.filename):
                 filename = secure_filename(_file.filename)
-                _file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+                _file.save(os.path.join(upload_folder,filename))
                 upcount += 1
             else:
                 flask.flash('FILE NOT ALLOWED: {_file.filename}')
             flask.render_template('pdfmagic.html')
         flask.flash(f'FILES UPLOADED: {upcount}')
-        batch_scrape()
+
+        #run the scraper
+        batch_scrape(sid)
+        zip_files(sid)
+    
+        return flask.render_template('downloads.html')
+
     return flask.render_template('pdfmagic.html')
+
+@app.route('/download/')
+def download():
+    try:
+        sid = flask.session['sid']
+        return flask.send_file(os.path.join(app.config['SCRAPE_OUTPUT_FOLDER'],f'{sid}.zip'))
+    except Exception as e:
+        flask.flash(str(e))
